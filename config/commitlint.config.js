e47e5229b407c99ab17995a982ccbb68710defeb
Tom Heller <tom@cannabinder.com>
2019-10-06 21:36:33 -0700

    dont be an idiot and disable the commit requirements - cause they are way over powered

--- config/commitlint.config.js
+++ config/commitlint.config.js
@@ -1,6 +1,6 @@
 /**
  * @type {import('@commitlint/core').Config}
  */
-const config = { extends: ['@commitlint/config-conventional'] }
-
+// const config = { extends: ['@commitlint/config-conventional'] }
+const config = { extends: [] }
 module.exports = config
